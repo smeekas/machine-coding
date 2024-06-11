90a9e603ceeaa5ed1ebd2800198435375877e45d
smeekas <smeetapatel008@gmail.com>
2024-06-11 11:40:54 +0530

    vanilla tictactoe: configurable winning condition

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -3,7 +3,8 @@ const control = document.querySelector("#control span");
 const winnerContainer = document.getElementById("winner");
 let currentControl = "X";
 let gameOver = false;
-const N = 5;
+const N = 4;
+const M = 3;
 let gameData = [];
 function createBoard() {
   board.innerHTML = "";
@@ -40,7 +41,7 @@ function tictactoe() {
         const isWinner = checkWinner();
         if (isWinner) {
           gameOver = isWinner;
-          winnerContainer.innerHTML = "Game Over";
+          winnerContainer.innerHTML = "Game Over" + currentControl + " Won";
           const button = document.createElement("button");
           button.innerHTML = "replay";
           button.addEventListener("click", () => {
@@ -54,32 +55,119 @@ function tictactoe() {
     }
   });
 }
-function checkWinner() {
-  for (let i = 0; i < N; i++) {
-    if (checkCols(i) || checkRows(i)) {
-      return true;
+function checkArray(arr) {
+  if (Array.isArray(arr)) {
+    let content = arr[0];
+    let count = 1;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] === content && content !== "") {
+        count++;
+      } else {
+        content = arr[i];
+        count = 1;
+      }
+
+      if (count === M) return true;
     }
+  }
+}
+function checkWinner() {
+  checkDiagonal();
+  if (checkRows() || checkCols() || checkDiagonal()) return true;
+  return false;
+}
+function checkRows() {
+  for (let i = 0; i < N; i++) {
+    if (checkArray[i]) return true;
   }
   return false;
 }
-function checkRows(index) {
-  let content = gameData[index][0];
-  if (content === "") return false;
-  for (let i = 1; i < N; i++) {
-    if (gameData[index][i] !== content) return false;
+
+function checkDiagonal() {
+  const diagonals = [];
+  // i=>row, j=>column
+  //back dia upper
+  for (let k = 0; k < N; k++) {
+    // upper to right with (top row)
+    let i = 0;
+    let j = k;
+    const arr = [];
+    while (i < N && j < N) {
+      arr.push(gameData[i][j]);
+      i++;
+      j++;
+    }
+    diagonals.push(arr);
   }
-  return true;
-}
-function checkCols(index) {
-  let content = gameData[0][index];
-  if (content === "") return false;
-  for (let i = 1; i < N; i++) {
-    if (gameData[i][index] !== content) return false;
+  //back dia left
+  for (let k = 1; k < N; k++) {
+    // upper to right with (first  column)
+
+    let i = k;
+    let j = 0;
+    const arr = [];
+    while (i < N && j < N) {
+      arr.push(gameData[i][j]);
+      i++;
+      j++;
+    }
+    diagonals.push(arr);
   }
-  return true;
+
+  //front dia right
+  for (let k = 0; k < N; k++) {
+    // upper to left with (last  column)
+
+    let i = k;
+    let j = N - 1;
+    const arr = [];
+    while (i < N && j >= 0) {
+      arr.push(gameData[i][j]);
+      i++;
+      j--;
+    }
+    diagonals.push(arr);
+  }
+
+  //front dia upper
+  for (let k = 0; k < N; k++) {
+    // upper to left with (first  row)
+
+    let i = 0;
+    let j = k;
+    const arr = [];
+    while (i < N && j >= 0) {
+      arr.push(gameData[i][j]);
+      i++;
+      j--;
+    }
+    diagonals.push(arr);
+  }
+  for (let i = 0; i < diagonals.length; i++) {
+    if (checkArray(diagonals[i])) return true;
+  }
+  return false;
 }
+
+function checkCols() {
+  const colData = gameData.reduce((acc, curr, index) => {
+    if (Array.isArray(curr)) {
+      curr.forEach((colItem, colIndex) => {
+        if (acc[colIndex] === undefined) acc[colIndex] = [];
+        acc[colIndex][index] = colItem;
+      });
+    }
+    return acc;
+  }, []);
+  for (let i = 0; i < N; i++) {
+    if (checkArray(colData[i])) return true;
+  }
+  return false;
+}
+
 function changeControl() {
   currentControl = currentControl === "X" ? "0" : "X";
   control.innerHTML = currentControl;
 }
+
 tictactoe();
